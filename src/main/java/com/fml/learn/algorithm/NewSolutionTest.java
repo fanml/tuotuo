15b2b12aec3d2dd35e17b096148b9bdc0adf9c15
fanml <fanmliang@yonyou.com>
2023-11-22 15:07:41 +0800

    LRU 缓存

--- src/main/java/com/fml/learn/algorithm/NewSolutionTest.java
+++ src/main/java/com/fml/learn/algorithm/NewSolutionTest.java
@@ -1,7 +1,9 @@
 package com.fml.learn.algorithm;
 
 import com.fml.learn.algorithm.structure.ListNode;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class NewSolutionTest {
@@ -66,5 +68,117 @@ public class NewSolutionTest {
     head.next = null;
     return newHead;
   }
+
+
+//  public class LRUCache {
+//
+//    /**
+//     * 算法
+//     * <p>
+//     * LRU 缓存机制可以通过哈希表辅以双向链表实现，我们用一个哈希表和一个双向链表维护所有在缓存中的键值对。
+//     * <p>
+//     * 双向链表按照被使用的顺序存储了这些键值对，靠近头部的键值对是最近使用的，而靠近尾部的键值对是最久未使用的。
+//     * <p>
+//     * 哈希表即为普通的哈希映射（HashMap），通过缓存数据的键映射到其在双向链表中的位置。
+//     * <p>
+//     * 这样以来，我们首先使用哈希表进行定位，找出缓存项在双向链表中的位置，随后将其移动到双向链表的头部，即可在 O(1)的时间内完成 get 或者 put 操作。具体的方法如下：
+//     * <p>
+//     * 对于 get 操作，首先判断 key 是否存在：
+//     * <p>
+//     * 如果 key 不存在，则返回 −1；
+//     * <p>
+//     * 如果 key 存在，则 key 对应的节点是最近被使用的节点。通过哈希表定位到该节点在双向链表中的位置，并将其移动到双向链表的头部，最后返回该节点的值。
+//     * <p>
+//     * 对于 put 操作，首先判断 key 是否存在：
+//     * <p>
+//     * 如果 key 不存在，使用 key 和 value 创建一个新的节点，在双向链表的头部添加该节点，并将 key 和该节点添加进哈希表中。然后判断双向链表的节点数是否超出容量，如果超出容量，则删除双向链表的尾部节点，并删除哈希表中对应的项；
+//     * <p>
+//     * 如果 key 存在，则与 get 操作类似，先通过哈希表定位，再将对应的节点的值更新为 value，并将该节点移到双向链表的头部。
+//     * <p>
+//     * 上述各项操作中，访问哈希表的时间复杂度为 O(1)，在双向链表的头部添加节点、在双向链表的尾部删除节点的复杂度也为 O(1)。
+//     * 而将一个节点移到双向链表的头部，可以分成「删除该节点」和「在双向链表的头部添加节点」两步操作，都可以在 O(1) 时间内完成。
+//     */
+//    class DLinkedNode {
+//
+//      int key;
+//      int value;
+//      DLinkedNode prev;
+//      DLinkedNode next;
+//
+//      public DLinkedNode() {
+//      }
+//
+//      public DLinkedNode(int _key, int _value) {
+//        key = _key;
+//        value = _value;
+//      }
+//    }
+//
+//    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+//    private int size;
+//    private int capacity;
+//    private DLinkedNode head, tail;
+//
+//    public LRUCache(int capacity) {
+//      this.size = 0;
+//      this.capacity = capacity;
+//      head = new DLinkedNode();
+//      tail = new DLinkedNode();
+//      head.next = tail;
+//      tail.prev = head;
+//    }
+//
+//    public int get(int key) {
+//      DLinkedNode node = cache.get(key);
+//      if (node == null) {
+//        return -1;
+//      }
+//      // 将node放在head后面
+//      moveToHead(node);
+//      return node.value;
+//    }
+//
+//    public void put(int key, int value) {
+//      DLinkedNode node = cache.get(key);
+//      if (node == null) {
+//        DLinkedNode nodeNew = new DLinkedNode(key, value);
+//        cache.put(key, nodeNew);
+//        addToHead(nodeNew);
+//        size++;
+//        if (size > capacity) {
+//          DLinkedNode node1 = removeTail();
+//          cache.remove(node1.key);
+//          --size;
+//        }
+//      } else {
+//        node.value = value;
+//        moveToHead(node);
+//      }
+//    }
+//
+//    private void addToHead(DLinkedNode node) {
+//      node.next = head.next;
+//      node.prev = head;
+//      head.next = node;
+//      head.next.prev = node;
+//    }
+//
+//    private void removeNode(DLinkedNode node) {
+//      node.prev.next = node.next;
+//      node.next.prev = node.prev;
+//    }
+//
+//    private void moveToHead(DLinkedNode node) {
+//      removeNode(node);
+//      addToHead(node);
+//    }
+//
+//    private DLinkedNode removeTail() {
+//      DLinkedNode node = tail.prev;
+//      removeNode(node);
+//      return node;
+//    }
+//  }
+//
 
 }
---
+++ src/main/java/com/fml/learn/algorithm/structure/LinkedNode.java
@@ -0,0 +1,8 @@
+package com.fml.learn.algorithm.structure;
+
+/**
+ * 简单的双链表
+ */
+public class LinkedNode {
+
+}
