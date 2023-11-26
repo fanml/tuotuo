108987fb89ac9ef6651fd1809fc05f5c3b3cfa6f
fanml <fanmliang@yonyou.com>
2023-11-26 21:29:42 +0800

    25. K 个一组翻转链表

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -266,6 +266,54 @@ public class NewSolution {
     return quickselect(_nums, 0, n - 1, n - k);
   }
 
+  /**
+   * 25. K 个一组翻转链表
+   *
+   *给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+   * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+   * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+   */
+    public ListNode reverseKGroup(ListNode head, int k) {
+      ListNode hair = new ListNode(0);
+      hair.next = head;
+      ListNode pre = hair;
+
+      while (head != null) {
+        ListNode tail = pre;
+        // 查看剩余部分长度是否大于等于 k
+        for (int i = 0; i < k; ++i) {
+          tail = tail.next;
+          if (tail == null) {
+            return hair.next;
+          }
+        }
+        ListNode nex = tail.next;
+        ListNode[] reverse = myReverse(head, tail);
+        head = reverse[0];
+        tail = reverse[1];
+        // 把子链表重新接回原链表
+        pre.next = head;
+        tail.next = nex;
+        pre = tail;
+        head = tail.next;
+      }
+
+      return hair.next;
+    }
+
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+      // tail的下一个节点
+      ListNode prev = tail.next;
+      // head节点
+      ListNode p = head;
+      while (prev != tail) {
+        ListNode nex = p.next;
+        p.next = prev;
+        prev = p;
+        p = nex;
+      }
+      return new ListNode[]{tail, head};
+    }
 
 
 
