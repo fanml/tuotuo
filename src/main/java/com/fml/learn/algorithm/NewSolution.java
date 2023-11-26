6633efe4589ff6ab998e7ea3e5963ea59ec81f49
fanml <fanmliang@yonyou.com>
2023-11-26 18:50:27 +0800

    215. 数组中的第K个最大元素

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1,6 +1,7 @@
 package com.fml.learn.algorithm;
 
 import com.fml.learn.algorithm.structure.ListNode;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -215,11 +216,66 @@ public class NewSolution {
   }
 
 
+  /**
+   * 215. 数组中的第K个最大元素
+   * 中等
+   * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+   *
+   * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+   *
+   * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+   *
+   * 示例 1:
+   * 输入: [3,2,1,5,6,4], k = 2
+   * 输出: 5
+   *
+   * 示例 2:
+   * 输入: [3,2,3,1,2,4,5,5,6], k = 4
+   * 输出: 4
+   *
+   */
+   static  int quickselect(int[] nums, int l, int r, int k) {
+    if (l == r) {
+      return nums[k];
+    }
+    int x = nums[l], i = l - 1, j = r + 1;
+    while (i < j) {
+      do {
+        i++;
+      } while (nums[i] < x);
+      do {
+        j--;
+      } while (nums[j] > x);
+      if (i < j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+      }
+    }
+    // 打印
+     System.out.println(Arrays.toString(nums));
+    if (k <= j) {
+      return quickselect(nums, l, j, k);
+    } else {
+      return quickselect(nums, j + 1, r, k);
+    }
+  }
+
+  public static int findKthLargest(int[] _nums, int k) {
+    int n = _nums.length;
+    return quickselect(_nums, 0, n - 1, n - k);
+  }
+
+
+
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
-    String s = "pwwkew";
-    System.out.println(lengthOfLongestSubstring(s));
+//    String s = "pwwkew";
+//    System.out.println(lengthOfLongestSubstring(s));
+    int [] nums ={3,2,1,5,6,4};
+    findKthLargest(nums,2);
 
 
   }
