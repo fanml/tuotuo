cdad6a74cd3644a2c0b3ec59884810f1be859eb0
fanml <fanmliang@yonyou.com>
2023-11-29 17:13:12 +0800

    121. 买卖股票的最佳时机

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -600,6 +600,33 @@ public class NewSolution {
   }
 
 
+  /**
+   * 121. 买卖股票的最佳时机
+   * 示例 2：
+   * 输入：[7,1,5,3,6,4]
+   * 输出：5
+   * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+   *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+   * 示例 2：
+   * 输入：prices = [7,6,4,3,1]
+   * 输出：0
+   * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+   *
+   * 动态规划
+   */
+  public int maxProfit(int[] prices) {
+    int min = Integer.MAX_VALUE;
+    int max = 0;
+    for(int i = 0;i<prices.length;i++){
+      if (min>prices[i]){
+        min = prices[i];
+      }else if(prices[i]-min >max) {
+        max = prices[i]-min;
+      }
+    }
+    return max;
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
