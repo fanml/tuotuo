c71b4859ca283ad37732617047938b6f51bd0cd9
fanml <fanmliang@yonyou.com>
2023-11-29 09:26:22 +0800

    102.二叉树的层序遍历

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1,12 +1,15 @@
 package com.fml.learn.algorithm;
 
 import com.fml.learn.algorithm.structure.ListNode;
+import com.fml.learn.algorithm.structure.TreeNode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -514,6 +517,35 @@ public class NewSolution {
     return right - left - 1;
   }
 
+
+  /**
+   * 102.二叉树的层序遍历 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。 输入：root =
+   * [3,9,20,null,null,15,7] 输出：[[3],[9,20],[15,7]]
+   */
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      List<Integer> list = new ArrayList<>();
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        list.add(node.val);
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+      result.add(list);
+    }
+    return result;
+  }
 
   public static void main(String[] args) {
 //    String s = "abcabcbb";
