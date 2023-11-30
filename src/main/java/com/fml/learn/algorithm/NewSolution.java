6f1d174bd1ae054447e4104e86f874e7bd5e013c
fanml <fanmliang@yonyou.com>
2023-11-30 15:43:22 +0800

    200. 岛屿数量

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -671,6 +671,85 @@ public class NewSolution {
   }
 
 
+  /**
+   * 200. 岛屿数量 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+   * 此外，你可以假设该网格的四条边均被水包围。 深度优先搜索
+   */
+  public int numIslandsDfs(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+    int rn = grid.length;
+    int cn = grid[0].length;
+    int result = 0;
+    for (int r = 0; r < rn; r++) {
+      for (int c = 0; c < cn; c++) {
+        if (grid[r][c] == '1') {
+          result++;
+          dfs(grid, r, c);
+        }
+      }
+    }
+    return result;
+  }
+
+  void dfs(char[][] grid, int r, int c) {
+    int rn = grid.length;
+    int cn = grid[0].length;
+    if (r < 0 || r >= rn || c < 0 || c >= cn || grid[r][c] == '0') {
+      return;
+    }
+    grid[r][c] = '0';
+    dfs(grid, r + 1, c);
+    dfs(grid, r - 1, c);
+    dfs(grid, r, c + 1);
+    dfs(grid, r, c - 1);
+  }
+
+  /**
+   * 200. 岛屿数量 广度优先搜索BFS
+   */
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int result = 0;
+    for (int r = 0; r < nr; r++) {
+      for (int c = 0; c < nc; c++) {
+        if (grid[r][c] == '1') {
+          result++;
+          grid[r][c] = '0';
+          Queue<Integer> neighbors = new LinkedList<>();
+          neighbors.add(r * nc + c);
+          while (!neighbors.isEmpty()) {
+            int id = neighbors.remove();
+            int row = id / nc;
+            int col = id % nc;
+            if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+              neighbors.add((row - 1) * nc + col);
+              grid[row - 1][col] = '0';
+            }
+            if (row + 1 < nr && grid[row + 1][col] == '1') {
+              neighbors.add((row + 1) * nc + col);
+              grid[row + 1][col] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+              neighbors.add(row * nc + col - 1);
+              grid[row][col - 1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col + 1] == '1') {
+              neighbors.add(row * nc + col + 1);
+              grid[row][col + 1] = '0';
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
