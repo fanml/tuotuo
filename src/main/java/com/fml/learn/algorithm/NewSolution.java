4ebc5f428ed1d9adf12f67ee145dadfa723d9282
fanml <fanmliang@yonyou.com>
2023-12-03 18:17:06 +0800

    46 全排列

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -4,6 +4,7 @@ import com.fml.learn.algorithm.structure.ListNode;
 import com.fml.learn.algorithm.structure.TreeNode;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -873,6 +874,42 @@ public class NewSolution {
     arrays[l] = pivot;
     quickSort(arrays, left, l - 1);
     quickSort(arrays, l + 1, right);
+  }
+
+  /**
+   * 46. 全排列
+   * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案
+   * 输入：nums = [1,2,3]
+   * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+   *
+   * 回溯
+   */
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    List<Integer> output = new ArrayList<Integer>();
+    for (int num : nums) {
+      output.add(num);
+    }
+
+    int n = nums.length;
+    backtrack(n, output, res, 0);
+    return res;
+  }
+
+  public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+    // 所有数都填完了
+    if (first == n) {
+      res.add(new ArrayList<Integer>(output));
+    }
+    for (int i = first; i < n; i++) {
+      // 动态维护数组
+      Collections.swap(output, first, i);
+      // 继续递归填下一个数
+      backtrack(n, output, res, first + 1);
+      // 撤销操作
+      Collections.swap(output, first, i);
+    }
   }
 
 
