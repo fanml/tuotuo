e80ccf160ba76ae1dd46fe1d17012ce09673ecf2
fanml <fanmliang@yonyou.com>
2023-12-05 22:04:39 +0800

    415. 字符串相加

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1084,6 +1084,30 @@ public class NewSolution {
 
   }
 
+  /**
+   * 415. 字符串相加
+   * <p>
+   * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+   * <p>
+   * 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+   */
+  public String addStrings(String num1, String num2) {
+    int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+    StringBuffer ans = new StringBuffer();
+    while (i >= 0 || j >= 0 || add != 0) {
+      int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+      int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+      int result = x + y + add;
+      ans.append(result % 10);
+      add = result / 10;
+      i--;
+      j--;
+    }
+    // 计算完以后的答案需要翻转过来
+    ans.reverse();
+    return ans.toString();
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
