df8e8a27928c354cc3bead23922cbb4761c69f5c
fanml <fanmliang@yonyou.com>
2023-12-05 22:42:22 +0800

    300. 最长递增子序列

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1108,6 +1108,68 @@ public class NewSolution {
     return ans.toString();
   }
 
+  /**
+   * 300. 最长递增子序列 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+   * <p>
+   * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+   * <p>
+   * 示例 1：
+   * <p>
+   * 输入：nums = [10,9,2,5,3,7,101,18] 输出：4 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+   * <p>
+   * 动态规划
+   */
+  public int lengthOfLIS(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    int[] dp = new int[nums.length];
+    dp[0] = 1;
+    int maxans = 1;
+    for (int i = 1; i < nums.length; i++) {
+      dp[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      maxans = Math.max(maxans, dp[i]);
+    }
+    return maxans;
+  }
+
+  /**
+   * 300. 最长递增子序列
+   *
+   * 贪心+二分 没学会抄一遍吧
+   */
+  public int lengthOfLIS1(int[] nums) {
+    int len = 1, n = nums.length;
+    if (n == 0) {
+      return 0;
+    }
+    int[] d = new int[n + 1];
+    d[len] = nums[0];
+    for (int i = 1; i < n; ++i) {
+      if (nums[i] > d[len]) {
+        d[++len] = nums[i];
+      } else {
+        int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+        while (l <= r) {
+          int mid = (l + r) >> 1;
+          if (d[mid] < nums[i]) {
+            pos = mid;
+            l = mid + 1;
+          } else {
+            r = mid - 1;
+          }
+        }
+        d[pos + 1] = nums[i];
+      }
+    }
+    return len;
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
