d4860822c523352af2bfbc7e114630c65411786b
fanml <fanmliang@yonyou.com>
2023-12-08 20:36:27 +0800

    最长公共子序列

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import org.springframework.util.StringUtils;
 
 /**
  * gogogo 2023-11-20
@@ -1463,6 +1464,65 @@ public class NewSolution {
       }
     }
     return ans;
+  }
+
+  /**
+   * 最长公共子串
+   * <p>
+   * 有两个字符串，s1="people"和s2="eplm"，我们要求他俩最长的公共子串。我们一眼就能看 出他们的最长公共子串是"pl"，长度是2
+   */
+  public int longestCommonSubString(String text1, String text2) {
+    if (text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
+      return 0;
+    }
+    int max = 0;
+    // 边界值加1
+    int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+    for (int i = 1; i <= text1.length(); i++) {
+      for (int j = 1; j <= text2.length(); j++) {
+        if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1] + 1; // 边界值加1原因 如果不加1 dp[-1][-1]越界
+        } else {
+          dp[i][j] = 0;
+        }
+        max = Math.max(dp[i][j], max);
+      }
+    }
+    return max;
+  }
+
+
+  /**
+   * 1143. 最长公共子序列
+   * <p>
+   * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+   * <p>
+   * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+   * <p>
+   * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。 示例 1：
+   * <p>
+   * 输入：text1 = "abcde", text2 = "ace" 输出：3 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+   * <p>
+   * <p>
+   * 动态规划
+   */
+  public int longestCommonSubsequence(String text1, String text2) {
+    if (text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
+      return 0;
+    }
+    int max = 0;
+    int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+    for (int i = 1; i <= text1.length(); i++) {
+      for (int j = 1; j <= text2.length(); j++) {
+        if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        }
+        max = Math.max(dp[i][j], max);
+      }
+    }
+    return max;
   }
 
   public static void main(String[] args) {
