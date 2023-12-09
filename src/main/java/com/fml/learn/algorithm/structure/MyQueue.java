931d4e9a30969387dfe39fc001c057f585692791
fanml <fanmliang@yonyou.com>
2023-12-09 21:55:55 +0800

    232. 用栈实现队列

---
+++ src/main/java/com/fml/learn/algorithm/structure/MyQueue.java
@@ -0,0 +1,49 @@
+package com.fml.learn.algorithm.structure;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 232. 用栈实现队列
+ */
+public class MyQueue {
+
+  Deque<Integer> inStack;
+  Deque<Integer> outStack;
+
+  public MyQueue() {
+    inStack = new ArrayDeque<>();
+    outStack = new ArrayDeque<>();
+  }
+
+  public void push(int x) {
+    inStack.push(x);
+  }
+
+  public int pop() {
+    if (outStack.isEmpty()) {
+      in2out();
+    }
+    return outStack.pop();
+
+  }
+
+  public int peek() {
+    if (outStack.isEmpty()) {
+      in2out();
+    }
+    return outStack.peek();
+  }
+
+  public boolean empty() {
+    return inStack.isEmpty() && outStack.isEmpty();
+
+  }
+
+  private void in2out() {
+    while (!inStack.isEmpty()) {
+      outStack.push(inStack.pop());
+    }
+  }
+
+}
