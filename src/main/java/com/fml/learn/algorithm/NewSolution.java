401578f1efc7243816b5637f60b33ba072d9301e
fanml <fanmliang@yonyou.com>
2023-12-10 22:39:33 +0800

    70. 爬楼梯

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1636,6 +1636,23 @@ public class NewSolution {
     return ans;
   }
 
+  /**
+   * 70. 爬楼梯
+   * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+   *
+   * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+   */
+  public int climbStairs(int n) {
+    // f(n) = f(n-1)+f(n+1);
+    int p = 0,q=0,r=1;
+    for(int i = 1;i<=n;i++){
+      p=q;
+      q=r;
+      r=p+q;
+    }
+    return r;
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
