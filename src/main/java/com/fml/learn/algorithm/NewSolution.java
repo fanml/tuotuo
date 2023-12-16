290d4f2c6677080e12f47c1a55f6b78736b19d9f
fanml <fanmliang@yonyou.com>
2023-12-16 22:32:09 +0800

    101. 对称二叉树

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2229,6 +2229,23 @@ public class NewSolution {
     }
   }
 
+  /**
+   * 101. 对称二叉树 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+   */
+  public boolean isSymmetric(TreeNode root) {
+    return check(root, root);
+  }
+
+  public boolean check(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+  }
+
 
   public static void main(String[] args) {
 //    String s = "abcabcbb";
