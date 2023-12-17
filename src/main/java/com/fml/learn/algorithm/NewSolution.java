1c562dcc7ab4c103ac940e0d8a3c099d533f8eda
fanml <fanmliang@yonyou.com>
2023-12-17 20:39:01 +0800

    543. 二叉树的直径

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2229,6 +2229,33 @@ public class NewSolution {
     }
   }
 
+  int ans;
+
+  /**
+   * 543. 二叉树的直径 给你一棵二叉树的根节点，返回该树的 直径 。
+   * <p>
+   * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+   * <p>
+   * 两节点之间路径的 长度 由它们之间边数表示。
+   * <p>
+   * 输入：root = [1,2,3,4,5] 输出：3 解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+   */
+  public int diameterOfBinaryTree(TreeNode root) {
+    ans = 1;
+    depth(root);
+    return ans - 1;
+  }
+
+  public int depth(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+    int l = depth(node.left);
+    int r = depth(node.right);
+    ans = Math.max(l + r + 1, ans);
+    return Math.max(l, r) + 1;
+  }
+
   /**
    * 101. 对称二叉树 给你一个二叉树的根节点 root ， 检查它是否轴对称。
    */
