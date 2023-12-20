e324476a415db8ac5132a1de8da18311e5c6d2b3
fanml <fanmliang@yonyou.com>
2023-12-20 14:28:28 +0800

    240. 搜索二维矩阵 II

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2554,6 +2554,30 @@ public class NewSolution {
         targetSum - root.val);
   }
 
+  /**
+   * 240. 搜索二维矩阵 II
+   * <p>
+   * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+   * <p>
+   * 每行的元素从左到右升序排列。 每列的元素从上到下升序排列。 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+   * target = 5 输出：true
+   */
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length, n = matrix[0].length;
+    int x = 0, y = n - 1;
+    while (x < m && y >= 0) {
+      if (matrix[x][y] == target) {
+        return true;
+      }
+      if (matrix[x][y] > target) {
+        --y;
+      } else {
+        ++x;
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
