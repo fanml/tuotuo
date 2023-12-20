c01d06c52789632e20017faa023c41a6227fa55b
fanml <fanmliang@yonyou.com>
2023-12-20 22:46:01 +0800

    62. 不同路径

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2579,8 +2579,7 @@ public class NewSolution {
   }
 
   /**
-   * 226. 翻转二叉树
-   * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+   * 226. 翻转二叉树 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
    */
   public TreeNode invertTree(TreeNode root) {
     if (root == null) {
@@ -2591,6 +2590,31 @@ public class NewSolution {
     root.left = right;
     root.right = left;
     return root;
+  }
+
+  /**
+   * 62. 不同路径
+   * <p>
+   * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+   * <p>
+   * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+   * <p>
+   * 问总共有多少条不同的路径？
+   */
+  public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
+    }
+    for (int i = 0; i < n; i++) {
+      dp[0][i] = 1;
+    }
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+      }
+    }
+    return dp[m - 1][n - 1];
   }
 
   public static void main(String[] args) {
