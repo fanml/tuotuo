5c9273d2131dfd73af64f3f96b9cb3035a0e40c2
fanml <fanmliang@yonyou.com>
2023-12-21 22:14:26 +0800

    14. 最长公共前缀

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2651,6 +2651,30 @@ public class NewSolution {
     return ans;
   }
 
+  /**
+   * 14. 最长公共前缀
+   * <p>
+   * 编写一个函数来查找字符串数组中的最长公共前缀。
+   * <p>
+   * 如果不存在公共前缀，返回空字符串 ""。
+   * <p>
+   * 输入：strs = ["flower","flow","flight"] 输出："fl"
+   */
+  public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0) {
+      return "";
+    }
+    for (int i = 0; i < strs[0].length(); i++) {
+      char c = strs[0].charAt(i);
+      for (int j = 1; j < strs.length; j++) {
+        if (strs[j].length() == i || strs[j].charAt(i) != c) {
+          return strs[0].substring(0, i);
+        }
+      }
+    }
+    return strs[0];
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
