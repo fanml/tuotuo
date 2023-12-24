0518ff46c083f9f052931b5967576532d2c66c0e
fanml <fanmliang@yonyou.com>
2023-12-24 20:48:21 +0800

    695. 岛屿的最大面积

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -990,6 +990,54 @@ public class NewSolution {
     return result;
   }
 
+
+  /**
+   * 695. 岛屿的最大面积
+   * <p>
+   * 给你一个大小为 m x n 的二进制矩阵 grid 。
+   * <p>
+   * 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+   * <p>
+   * 岛屿的面积是岛上值为 1 的单元格的数目。
+   * <p>
+   * 计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+   */
+  public int maxAreaOfIsland(int[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return ansM;
+    }
+    int row = grid.length;
+    int cloumn = grid[0].length;
+    for (int r = 0; r < row; r++) {
+      for (int c = 0; c < cloumn; c++) {
+        if (grid[r][c] == 1) {
+          dfs(grid, r, c);
+          ansM = Math.max(ansM, tempM);
+          tempM = 0;
+        }
+      }
+    }
+    return ansM;
+  }
+
+  int ansM = 0;
+  int tempM = 0;
+
+  private void dfs(int[][] grid, int r, int c) {
+    int row = grid.length;
+    int cloumn = grid[0].length;
+    if (r < 0 || r >= row || c < 0 || c >= cloumn || grid[r][c] == 0) {
+      return;
+    }
+    grid[r][c] = 0;
+    tempM++;
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+
+
   /**
    * 88. 合并两个有序数组
    * <p>
