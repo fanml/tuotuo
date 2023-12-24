4d9a4c5f2d0074a7bd738588131574111795d595
fanml <fanmliang@yonyou.com>
2023-12-24 22:14:04 +0800

    227. 基本计算器 II

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -1761,11 +1761,10 @@ public class NewSolution {
 
   /**
    * 83. 删除排序链表中的重复元素
-   *
+   * <p>
    * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
-   *
-   * 输入：head = [1,1,2]
-   * 输出：[1,2]
+   * <p>
+   * 输入：head = [1,1,2] 输出：[1,2]
    */
   public ListNode deleteDuplicates1(ListNode head) {
     if (head == null) {
@@ -2764,6 +2763,53 @@ public class NewSolution {
       }
     }
     return strs[0];
+  }
+
+  /**
+   * 227. 基本计算器 II
+   * <p>
+   * * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+   * <p>
+   * 整数除法仅保留整数部分。
+   * <p>
+   * 你可以假设给定的表达式总是有效的。所有中间结果将在 [-231, 231 - 1] 的范围内。
+   * <p>
+   * 注意：不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+   * <p>
+   * 输入：s = "3+2*2" 输出：7
+   */
+  public int calculate(String s) {
+    Deque<Integer> stack = new ArrayDeque<Integer>();
+    char preSign = '+';
+    int num = 0;
+    int n = s.length();
+    for (int i = 0; i < n; ++i) {
+      if (Character.isDigit(s.charAt(i))) {
+        num = num * 10 + s.charAt(i) - '0';
+      }
+      if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+        switch (preSign) {
+          case '+':
+            stack.push(num);
+            break;
+          case '-':
+            stack.push(-num);
+            break;
+          case '*':
+            stack.push(stack.pop() * num);
+            break;
+          default:
+            stack.push(stack.pop() / num);
+        }
+        preSign = s.charAt(i);
+        num = 0;
+      }
+    }
+    int ans = 0;
+    while (!stack.isEmpty()) {
+      ans += stack.pop();
+    }
+    return ans;
   }
 
   public static void main(String[] args) {
