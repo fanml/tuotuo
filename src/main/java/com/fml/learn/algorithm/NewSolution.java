d0725614c68ece8913bb3fa546fab0c7ed0ea0b7
fanml <fanmliang@yonyou.com>
2023-12-25 10:49:20 +0800

    139. 单词拆分

--- src/main/java/com/fml/learn/algorithm/NewSolution.java
+++ src/main/java/com/fml/learn/algorithm/NewSolution.java
@@ -2812,6 +2812,31 @@ public class NewSolution {
     return ans;
   }
 
+  /**
+   * 139. 单词拆分 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+   * <p>
+   * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+   * <p>
+   * 示例 1：
+   * <p>
+   * 输入: s = "leetcode", wordDict = ["leet", "code"] 输出: true 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和
+   * "code" 拼接成。
+   */
+  public boolean wordBreak(String s, List<String> wordDict) {
+    Set<String> wordDictSet = new HashSet(wordDict);
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[0] = true;
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 0; j < i; j++) {
+        if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[s.length()];
+  }
+
   public static void main(String[] args) {
 //    String s = "abcabcbb";
 //    String s = "bbbbb";
